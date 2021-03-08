248fcc521af7bc29223221ea31bce0a09837acbb
chrisrappa <rappaproductionsinc@gmail.com>
2021-03-08 13:27:04 -0600

    publish

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,10 @@ app.get('/api/config/paypal', (req, res) => {
 })
 
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
   });
 
-app.listen (config.PORT, () => {
+app.listen (process.env.PORT || 5000, () => {
     console.log ("Server started at http://localhost:5000")
 })
