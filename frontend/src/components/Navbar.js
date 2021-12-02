e9e16c3994cd3d362b619a41213e63078d93eb28
chrisrappa <rappaproductionsinc@gmail.com>
2021-12-01 19:26:48 -0600

    menubutton

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -51,17 +51,11 @@ function Navbar(props) {
         <div className="relative flex items-center justify-between h-16">
           
           {/* Mobile Menu Button */}
-          <div onClick = {() => toggleMenu()} className="mobile-button absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
+            <button onClick = {() => toggleMenu()} type="button" className="inline-flex items-center justify-center p-4 rounded-md text-gray-400">
               <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
-              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
             </button>
-          </div>
           
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center w-50">
