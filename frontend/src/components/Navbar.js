3c82195ddaa98471428a35b4c590ffda06cae575
chrisrappa <rappaproductionsinc@gmail.com>
2021-12-02 14:16:39 -0600

    publish

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -51,7 +51,7 @@ function Navbar(props) {
         <div className="relative flex items-center justify-between h-16">
           
           {/* Mobile Menu Button */}
-          <button onClick = {() => toggleMenu()} type="button" className="w-20 h-full inline-flex items-center justify-center p-2 rounded-md text-gray-400 md:hidden lg:hidden xl:hidden">
+          <button onClick = {() => toggleMenu()} type="button" className="w-20 h-full inline-flex items-center justify-center p-2 rounded-md text-gray-400 md:hidden lg:hidden xl:hidden cursor-pointer">
             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"  stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
